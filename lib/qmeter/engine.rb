42d91df5c4b186a7e2d9f4a61e27e08b6571bf51
arvind-clarion <arvind.vyas@clariontechnologies.co.in>
2015-06-04 13:41:00 +0530

    issue #1 Fixed Added qmeter assets to application

--- lib/qmeter/engine.rb
+++ lib/qmeter/engine.rb
@@ -1,3 +1,8 @@
 module Qmeter
-  class Engine < Rails::Engine; end
+  class Engine < Rails::Engine
+    ### @arvind : Added css and js file to assets as it require for qmeter page ###
+    initializer "qmeter.assets.precompile" do |app|
+      app.config.assets.precompile += %w( qmeter/qmeter.css  qmeter/jquery-v1.11.2.js qmeter/jquery-v1.11.2.js)
+    end
+  end
 end
