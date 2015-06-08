06682b5b7111e6fa80f5c9871076b0d1585c5650
arvind-clarion <arvind.vyas@clariontechnologies.co.in>
2015-06-08 11:48:25 +0530

    issue #1 fixed typo

--- lib/qmeter/engine.rb
+++ lib/qmeter/engine.rb
@@ -2,7 +2,7 @@ module Qmeter
   class Engine < Rails::Engine
     ### @arvind : Added css and js file to assets as it require for qmeter page ###
     initializer "qmeter.assets.precompile" do |app|
-      app.config.assets.precompile += %w( qmeter/qmeter.css  qmeter/jquery-v1.11.2.js qmeter/jquery-v1.11.2.js)
+      app.config.assets.precompile += %w( qmeter/qmeter.css  qmeter/jquery-v1.11.2.js qmeter/qmeter.js)
     end
   end
 end
